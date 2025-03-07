18a8bc0424d7023da1412dfaf9b0bcd361a4ba3f
一个快乐的小兔子 <14697957+a-happy-little-rabbit@user.noreply.gitee.com>
2025-03-07 16:42:59 +0800

    add cert file

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
-app.use(express.json());
 
+// 读取证书和私钥文件
+const options = {
+  key: fs.readFileSync("D://OpenSSL-Win64//bin//key.pem"),
+  cert: fs.readFileSync("D://OpenSSL-Win64//bin//PEM//cert.pem"),
+};
+
+// 定义路由
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello, HTTPS!");
 });
-app.listen(3007, () => {
-  console.log("Server is running on port 3000", "https://localhost:3007/");
+
+// 创建 HTTPS 服务器
+const server = https.createServer(options, app);
+
+// 启动服务器
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on https://localhost:${port}`);
 });
